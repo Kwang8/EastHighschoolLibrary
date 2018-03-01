4f64ca63cc6343463bce74e1af219dfc25056cac
Kevin Wang <kevinwang@kevins-macbook-air.local>
2018-03-01 15:14:35 -0500

    first Commit

---
+++ TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  EastHighschoolLibrary
+//
+//  Created by Kevin Wang on 2/28/18.
+//  Copyright © 2018 Kevin Wang. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var stockLabel: UILabel!
+    @IBOutlet weak var bookNameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
